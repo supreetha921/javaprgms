8a748ddd460e6e0657c77e4b3662be2d34604e00
supreetha <gcsupritha22@gmail.com>
2021-06-28 11:02:38 +0530

    using arrays and method programs

---
+++ MaxNumber.java
@@ -0,0 +1,16 @@
+public class MaxNumber{
+
+  public static void main(String[] args) {
+
+      int num1 = 10, num2 = 30, num3 = 15;
+
+      if( num1 >= num2 && num1 >= num3)
+          System.out.println(num1+" is the maximum Number");
+
+      else if (num2 >= num1 && num2 >= num3)
+          System.out.println(num2+" is the maximum Number");
+
+      else
+          System.out.println(num3+" is the maximum Number");
+  }
+}
---
+++ MinNumber.java
@@ -0,0 +1,18 @@
+class MinNumber {
+   
+ 
+ 
+  public static void main (String[] args) {
+            int a = 5, b = 7, c = 10;
+ 
+    if (a <= b && a <= c)
+        System.out.println( a + " is the smallest");
+ 
+    else if (b <= a && b <= c)
+        System.out.println( b + " is the smallest");
+ 
+    else
+        System.out.println( c + " is the smallest");
+ 
+    }
+}

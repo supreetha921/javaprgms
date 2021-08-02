430ee030abec728050f1581d2b7a2d4103921792
supreetha921 <gcsupritha22@gmail.com>
2021-08-02 13:18:23 +0530

    Programs worked on Eclipse

---
+++ evenodd/src/com/evenodd/evenoddapp/EvenOdd.java
@@ -0,0 +1,25 @@
+package com.evenodd.evenoddapp;
+
+public class EvenOdd {
+	
+	
+		
+	public static void main(String[] args) {
+
+	      int num=2;
+	   
+	       
+	      if(num % 2 == 0) {
+	        System.out.println(num + " is even");
+	        }
+	        else
+	          System.out.println(num + " is odd");
+	    }
+		
+		
+		
+
+	}
+
+
+

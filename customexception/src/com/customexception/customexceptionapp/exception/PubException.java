211fe493d0968e722eaf83f6a71a1697e311b9b8
supreetha921 <gcsupritha22@gmail.com>
2021-08-21 21:48:06 +0530

    programs on collection

--- customexception/src/com/customexception/customexceptionapp/exception/PubException.java
+++
@@ -1,9 +0,0 @@
-package com.customexception.customexceptionapp.exception;
-
-//Custom checked Exception
-public class PubException extends Exception {
-  
-	public String getMessage() {
-		return "you are too young to enter the PUB";
-	}
-}
